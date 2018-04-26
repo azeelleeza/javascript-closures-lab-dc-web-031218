8e29c268251b1865345b36e69f5c1f7abe559d83
azeelleeza <leezaluncheon@gmail.com>
2018-04-25 21:20:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+function bumpCounter(){
+  let counter=0;
+  // return (()=>{
+    const addBump = ()=>counter++;
+    const getBumps = ()=>counter;
+    return {addBump,getBumps};
+  // })();
+}
+
+function createAnimal(animalType){
+  return(deadlyDevice)=>(
+    {
+      'animalType': animalType,
+      'deadlyDevice': deadlyDevice
+    }
+  );
+}
+
+const sharkCreator = createAnimal("Shark");
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+const sharkWithFrickinCannon = sharkCreator("Cannon");
